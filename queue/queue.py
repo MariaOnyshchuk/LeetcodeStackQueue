166cdec8ba0256bf5b2fafb7a9e9d56580fe9bec
MariaOnyshchuk <onyshchuk.pn@ucu.edu.ua>
2024-04-13 15:23:04 +0300

    queue by linked list

--- queue/queue.py
+++ queue/queue.py
@@ -1,18 +1,31 @@
 class Node:
+    '''class node'''
     def __init__(self, value):
         self.value = value
         self.next = None
 
 class Queue:
+    '''class queue'''
     def __init__(self):
-        self.start = self.end = None #front node = strat; last node = end
+        '''init queue'''
+        self.head = None
 
     def push(self, value):
-        if value:
-            node = Node(value)
-            self.end.next = node
-            self.start.next = self.end
+        '''push node to queue'''
+        node = Node(value)
+        if not self.head:
+            self.head = node
+        else:
+            current = self.head
+            while current:
+                if not current.next:
+                    current.next = node
+                    break
+                current = current.next
 
-    def pull(self, value):
-
-        
+    def pull(self):
+        '''pull node from start'''
+        if self.head:
+            node = self.head
+            self.head = self.head.next
+            node.next = None
